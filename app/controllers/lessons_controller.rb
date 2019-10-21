71fa22fdfb3a2213a2d524df64786ab8aa8012f7
Dan <dseventeen@gmail.com>
2019-10-21 03:23:21 +0000

    enrolled to see course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson.section.course
   def show
   end
 end
 
   private
+
+  def require_authorized_for_current_lesson.section.course
+    if current_course.enrolled_in? != true
+      redirect_to /courses/:id(.:format), alert: 'To access the cours, please enroll first'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
