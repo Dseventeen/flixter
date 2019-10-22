6a3d6137eb95179eb71062769158c8bfd6a052b5
Dan <dseventeen@gmail.com>
2019-10-22 20:33:14 +0000

    sections ordering complete

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -41,6 +41,8 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
+    params.require(:section).permit(:title, :subtitle, :video, :row_order_position)
+
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
